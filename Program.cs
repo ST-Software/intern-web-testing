8f8bffcd81e74a796a25512c01ae00f3816dd423
Kratzer <jakub.kratzer@pslib.cz>
2016-05-24 11:20:19 +0200

    initial commit

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Selenium;
-using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
 using OpenQA.Selenium;
 
 
@@ -14,12 +14,24 @@ namespace web_testing
     {
         static void Main(string[] args)
         {
-            using (IWebDriver driver = new ChromeDriver())
+            using (IWebDriver driver = new FirefoxDriver())
             {
+                int counter = 1;
                 driver.Navigate().GoToUrl("http://10.0.1.228/intern_web_testing/Attendance.mvc/WorkTimePerDay");
                 IList<IWebElement> even = driver.FindElements(By.ClassName("even"));
                 IList<IWebElement> odd = driver.FindElements(By.ClassName("odd"));
-                
+                //foreach(var item in even)
+                //{
+                //    item.FindElements(By.LinkText()
+                //}
+                foreach (IWebElement option in even)
+                {
+                    Console.WriteLine(counter.ToString() +") " + option.Text);
+                    counter += 1;
+                    option.Click();
+                }
+                Console.ReadKey();
+
 
             }
         }
